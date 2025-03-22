3883d51a1feed6410acd4f22956444e5d543ce51
Gordon Beanman <skelebrine67@gmail.com>
2025-03-22 20:29:48 +0530

    small changes

---
+++ bar.js
@@ -0,0 +1,23 @@
+window.onload = function () {
+    var oldbar = document.getElementById("bar");
+    var newbar = document.createElement("div");
+
+    newbar.id = "bar";
+    newbar.innerHTML = `
+        <section class="tabs" style="max-width: 100%">
+            <menu role="tablist">
+                <button role="tab" aria-controls="tab-A" onclick=" window.location.assign('index.html')">Homepage</button>
+                <button role="tab" aria-controls="tab-B" onclick=" window.location.assign('socials.html')">Socials</button>
+                <button role="tab" aria-controls="tab-C" onclick=" window.location.assign('projects.html')">Projects</button>
+                <button role="tab" aria-controls="tab-D" onclick=" window.location.assign('old/warn.html')">Half Life</button>
+                <button role="tab" aria-controls="tab-E" disabled>Misc (coming soon)</button>
+            </menu>
+        </section>
+    `;
+
+    if (oldbar) {
+        oldbar.replaceWith(newbar);
+    } else {
+        console.warn("Element with ID 'bar' not found.");
+    }
+};
