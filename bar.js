4531324fa64ab277a0cff6bc1a1e92502b8fee7f
Gordon Beanman <skelebrine67@gmail.com>
2025-06-26 23:35:50 +0530

    MISC IS NOT WIP ANYMORE
    owo

--- bar.js
+++ bar.js
@@ -9,20 +9,20 @@ window.onload = function () {
 
     var oldbar = document.getElementById("bar");
     var newbar = document.createElement("div");
-
+    
      
 
 
     newbar.id = "bar";
     newbar.innerHTML = `
-   
+    
         <section class="tabs" style="max-width: 100%">
             <menu role="tablist">
                 <button role="tab" aria-controls="tab-A" onclick=" win('index.html')">Homepage</button>
                 <button role="tab" aria-controls="tab-B" onclick=" win('socials.html')">Socials</button>
                 <button role="tab" aria-controls="tab-C" onclick=" win('projects.html')">Projects</button>
                 <button role="tab" aria-controls="tab-D" onclick=" win('old/warn.html')">Half Life</button>
-                <button role="tab" aria-controls="tab-E" disabled>Misc (coming soon)</button>
+                <button role="tab" aria-controls="tab-E"onclick= "win('about.html')" >about/misc</button>
             </menu>
         </section>
 
--- global.js
+++
@@ -1,7 +0,0 @@
- function win(url) {
-var audio = new Audio('WINNAV.mp3');
-audio.play()
-audio.addEventListener('ended', () => {
-  window.location.assign(url);
-});
- }
