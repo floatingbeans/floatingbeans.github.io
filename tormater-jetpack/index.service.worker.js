8119b3ad3ac9455131139f303faccc7240336eb6
Gordon Beanman <130737378+floatingbeans@users.noreply.github.com>
2026-04-03 16:57:13 +0530

    MORE fixes

--- tormater-jetpack/index.service.worker.js
+++ tormater-jetpack/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1775213097|5995475707';
+const CACHE_VERSION = '1775215627|8525416551';
 /** @type {string} */
 const CACHE_PREFIX = 'Tormater: Jetpac-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
