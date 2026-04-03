58a87daa52fe5c7d659d376b8cf077ec8128647e
Gordon Beanman <130737378+floatingbeans@users.noreply.github.com>
2026-04-03 12:18:53 +0530

    game

--- secretgame/index.service.worker.js
+++ secretgame/index.service.worker.js
@@ -4,9 +4,9 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1775144523|5430603723';
+const CACHE_VERSION = '1775198925|841702584';
 /** @type {string} */
-const CACHE_PREFIX = 'Tormator Dash-sw-cache-';
+const CACHE_PREFIX = 'Tormater: Jetpac-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 /** @type {string} */
 const OFFLINE_URL = 'index.offline.html';
