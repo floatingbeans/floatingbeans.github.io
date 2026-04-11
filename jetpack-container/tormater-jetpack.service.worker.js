b5f7e9bbd9a0ef1990eeec0a63a688db8bf1d61f
Gordon Beanman <130737378+floatingbeans@users.noreply.github.com>
2026-04-11 23:24:01 +0530

    minor display adjustments. mraw

--- jetpack-container/tormater-jetpack.service.worker.js
+++ jetpack-container/tormater-jetpack.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1775926886|2232687667';
+const CACHE_VERSION = '1775930030|1054798619';
 /** @type {string} */
 const CACHE_PREFIX = 'Tormater Jetpack-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
