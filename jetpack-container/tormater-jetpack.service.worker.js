c19489e7b1bd2ddcd1ee4f05ef41311cb7f1732f
Gordon Beanman <130737378+floatingbeans@users.noreply.github.com>
2026-04-12 16:24:09 +0530

    ggooneregwbgew

--- jetpack-container/tormater-jetpack.service.worker.js
+++ jetpack-container/tormater-jetpack.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1775933015|549996801';
+const CACHE_VERSION = '1775991240|902175619';
 /** @type {string} */
 const CACHE_PREFIX = 'Tormater Jetpack-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
